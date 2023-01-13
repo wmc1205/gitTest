adb274f807820f2941c567c0a8f189026f8c05d7
우민철 <koi.mcw@gmail.com>
2023-01-13 14:23:59 +0900

    stach pop에 대한 test 중 - homeController> //stach test

--- src/main/java/koi_gitTest/HomeController.java
+++ src/main/java/koi_gitTest/HomeController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
 	public String hello() {
 		return "home";
 	}
+	//second commit - stached 파일 불러오는지?
 
 }
